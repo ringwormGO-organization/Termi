de63313859008d0711c67e966fe117e87ec851a7
StjepanBM1 <94230991+StjepanBM1@users.noreply.github.com>
2021-12-09 17:27:12 +0100

    Update main.cpp

--- Termi-Linux/main.cpp
+++ Termi-Linux/main.cpp
@@ -7,9 +7,14 @@ int main()
     int help = 1;
     switch (1){
     case 1:
-        cout << "There are no commands to help you with";
+        cout << "There are no commands to help you with" << endl;
+        cout << "But here's a cool triangle" << endl;
+        cout << "   /|" << endl;
+        cout << "  / |" << endl;
+        cout << " /  |" << endl;
+        cout << "/   |" << endl;
+        cout << "-----" << endl;
         break;
     }
     return 0;
 }
-
