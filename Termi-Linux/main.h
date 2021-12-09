ec2e27cf2ab58dbf47114f5a9cc09f48f5468ecc
StjepanBM1 <94230991+StjepanBM1@users.noreply.github.com>
2021-12-09 18:50:22 +0100

    Update main.h

--- Termi-Linux/main.h
+++ Termi-Linux/main.h
@@ -1,2 +1,8 @@
-my friend Andrej made me do this I personaly think that there is no point in header file for a programm who has to do 2 things
-If you disagree idc
+//my friend Andrej made me do this I personaly think that there is no point in header file for a programm who has to do 2 things
+//If you disagree idc
+int main()
+{
+  //random shit random shit
+  
+  return 0;
+}
