45dbf65ee6d3fa8013340b0ba94eff9cba35f3a9
StjepanBM1 <94230991+StjepanBM1@users.noreply.github.com>
2021-12-09 17:14:39 +0100

    Update Termi.cpp

--- Termi-Linux/Termi.cpp
+++ Termi-Linux/Termi.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int termi()
+{
+    int termi = 1;
+    switch (1){
+    case 1:
+        cout << "There are commands to help you with";
+        break;
+    }
+    return 0;
+}
 
