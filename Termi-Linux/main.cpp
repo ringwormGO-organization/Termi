1a6eed6ba7c0edcb61b80776ecce016aee0cca7d
StjepanBM1 <94230991+StjepanBM1@users.noreply.github.com>
2021-12-09 17:24:04 +0100

    Update main.cpp

--- Termi-Linux/main.cpp
+++ Termi-Linux/main.cpp
@@ -7,7 +7,7 @@ int main()
     int help = 1;
     switch (1){
     case 1:
-        cout << "There are commands to help you with";
+        cout << "There are no commands to help you with";
         break;
     }
     return 0;
