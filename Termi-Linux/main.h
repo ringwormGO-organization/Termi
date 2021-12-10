81ff6637ff00786c2bc54790ae3aadacea8d992b
Andrej Bartulin <andrej.bartulinvr@gmail.com>
2021-12-10 15:46:25 +0100

    Update main.h

--- Termi-Linux/main.h
+++ Termi-Linux/main.h
@@ -1,10 +1,2 @@
-//my friend Andrej made me do this I personaly think that there is no point in header file for a programm who has to do 2 things
-//If you disagree idc
-int main()
-{
-  
-  //there is no point in this
-  // But I'm doing it anyway
-  
-  return 0;
-}
+
+int main();
