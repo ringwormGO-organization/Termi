9efdd5dc758acf7f497d9dfcbd60844fdf5d7966
StjepanBM1 <94230991+StjepanBM1@users.noreply.github.com>
2021-12-11 09:27:56 +0100

    Update main.cpp

--- Termi-Linux/main.cpp
+++ Termi-Linux/main.cpp
@@ -4,10 +4,7 @@
 
 using namespace std;
 
-/̶/̶ ̶s̶h̶o̶u̶l̶d̶ ̶m̶a̶k̶e̶ ̶t̶h̶e̶ ̶c̶o̶d̶e̶ ̶p̶r̶e̶t̶t̶i̶e̶r̶ ̶(̶ ̶a̶n̶d̶ ̶i̶t̶ ̶s̶h̶o̶u̶l̶d̶ ̶p̶r̶o̶b̶ ̶b̶e̶ ̶m̶o̶r̶e̶ ̶o̶r̶g̶a̶n̶i̶z̶e̶d̶)̶ ̶b̶u̶t̶ ̶I̶ ̶g̶u̶e̶s̶s̶ ̶i̶t̶'̶s̶ ̶f̶i̶n̶e̶?̶ ̶
-/̶/̶ ̶P̶.̶S̶ ̶I̶ ̶s̶u̶c̶k̶ ̶a̶t̶ ̶o̶r̶g̶a̶n̶i̶z̶a̶t̶i̶o̶n̶ ̶
-/̶/̶ ̶-̶ ̶S̶t̶j̶e̶p̶a̶n̶B̶M̶1̶
-
+// I really should start putting stuff inside main.h
 
 int main()
 {
