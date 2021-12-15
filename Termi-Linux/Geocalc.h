9818c8b94fc9067c44bd31a0aac598a34ec44654
StjepanBM1 <94230991+StjepanBM1@users.noreply.github.com>
2021-12-15 11:48:38 +0100

    Create Geocalc.h

---
+++ Termi-Linux/Geocalc.h
@@ -0,0 +1,14 @@
+double opsgTrijustr(double x, double y, double z)
+{
+    return(x + y + z);
+}
+
+double opsgDvijustr(double x, double y)
+{
+    return(x + y);
+}
+
+double opsgKvdjustr(double x, int sqe)
+{
+    return(x*sqe);
+}
