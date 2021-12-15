8d5afd702680d48eb081f4cd41277f3f2e620b09
StjepanBM1 <94230991+StjepanBM1@users.noreply.github.com>
2021-12-15 11:49:44 +0100

    Create Calc.h

---
+++ Termi-Linux/Calc.h
@@ -0,0 +1,22 @@
+int Calc();
+
+double sumOfTwoNumbers(double x, double y)
+{
+    return (x + y);
+}
+
+double subOfTwoNumbers(double x, double y)
+{
+    return (x - y);
+}
+
+double mulOfTwoNumbers(double x, double y)
+{
+    return (x * y);
+}
+
+double devOfTwoNumbers(double x, double y)
+{
+    return (x / y);
+}
+
