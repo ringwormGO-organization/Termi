6be6764632d56fd2a6b1e98f0692b04e7afffcdc
StjepanBM1 <94230991+StjepanBM1@users.noreply.github.com>
2021-12-25 10:13:57 +0100

    Update Calc.h

--- Termi-Linux/Calc.h
+++ Termi-Linux/Calc.h
@@ -20,3 +20,27 @@ double devOfTwoNumbers(double x, double y)
     return (x / y);
 }
 
+double opsgTrijustr(double x, double y, double z)
+{
+    return(x + y + z);
+}
+
+double opsgDvijustr(double x, double y)
+{
+    return(x + x + y + y);
+}
+
+double opsgKvdjustr(double x, int sqe)
+{
+    return(x*sqe);
+}
+
+double povrsDvijustr(double x, double y)
+{
+    return(x*y);
+}
+
+double povrsKvdjustr(double x)
+{
+    return(x*x);
+}
