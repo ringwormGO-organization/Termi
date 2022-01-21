ac2b426b5d376ba7d55fb84a46f8ce06e8804748
Andrej Bartulin <andrej.bartulinvr@gmail.com>
2022-01-21 13:08:45 +0100

    Launcher update

---
+++ Termi-Windows/Termi-Launcher - Copy/MainWindow.xaml.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Net;
+using System.Windows;
+using System.Security.Principal;
+
+namespace Termi_Launcher
+{
+    enum LauncherStatus
+    {
+        ready,
+        failed,
+        downloadingTermi,
+        downloadingUpdate
+    }
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string rootPath;
+        private string versionFile;
+        private string TermiZip;
+        private string TermiExe;
+
+        private LauncherStatus _status;
+        internal LauncherStatus Status
+        {
+            get => _status;
+            set
+            {
+                _status = value;
+                switch (_status)
+                {
+                    case LauncherStatus.ready:
+                        PlayButton.Content = "Run Termi";
+                        break;
+                    case LauncherStatus.failed:
+                        PlayButton.Content = "Update Failed - Retry";
+                        break;
+                    case LauncherStatus.downloadingTermi:
+                        PlayButton.Content = "Downloading Termi";
+                        break;
+                    case LauncherStatus.downloadingUpdate:
+                        PlayButton.Content = "Downloading Update";
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        public static void IsAdministrator()
+        {
+            var identity = WindowsIdentity.GetCurrent();
+            var principal = new WindowsPrincipal(identity);
+            if (!principal.IsInRole(WindowsBuiltInRole.Administrator))
+            {
+                MessageBox.Show("You need to run this application by administrator!");
+                Environment.Exit(0);
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            rootPath = Directory.GetCurrentDirectory();
+            versionFile = Path.Combine(rootPath, "Version.txt");
+            TermiZip = Path.Combine(rootPath, "net5.0.zip");
+            TermiExe = Path.Combine(rootPath, "net5.0", "Termi-Runner-Console.exe");
+            IsAdministrator();
+        }
+
+        private void CheckForUpdates()
+        {
+            if (File.Exists(versionFile))
+            {
+                Version localVersion = new Version(File.ReadAllText(versionFile));
+                VersionText.Text = localVersion.ToString();
+
+                try
+                {
+                    WebClient webClient = new WebClient();
+                    Version onlineVersion = new Version(webClient.DownloadString("https://drive.google.com/uc?export=download&id=1yU_xMfNmCuh8xaRzqNVWqJMuxlGgl0qV")); //version
+
+                    if (onlineVersion.IsDifferentThan(localVersion))
+                    {
+                        InstallGameFiles(true, onlineVersion);
+                    }
+                    else
+                    {
+                        Status = LauncherStatus.ready;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Status = LauncherStatus.failed;
+                    MessageBox.Show($"Error checking for Termi updates: {ex}");
+                }
+            }
+            else
+            {
+                InstallGameFiles(false, Version.zero);
+            }
+        }
+
+        private void InstallGameFiles(bool _isUpdate, Version _onlineVersion)
+        {
+            try
+            {
+                WebClient webClient = new WebClient();
+                if (_isUpdate)
+                {
+                    Status = LauncherStatus.downloadingUpdate;
+                }
+                else
+                {
+                    Status = LauncherStatus.downloadingTermi;
+                    _onlineVersion = new Version(webClient.DownloadString("https://drive.google.com/uc?export=download&id=1yU_xMfNmCuh8xaRzqNVWqJMuxlGgl0qV")); //version
+                }
+
+                webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(DownloadGameCompletedCallback);
+                webClient.DownloadFileAsync(new Uri("https://drive.google.com/uc?export=download&id=1lsTAeZJiDXKkieRjuT7PfKLZ_IQMwr-s"), TermiZip, _onlineVersion); //zip
+            }
+            catch (Exception ex)
+            {
+                Status = LauncherStatus.failed;
+                MessageBox.Show($"Error installing Termi files: {ex}");
+            }
+        }
+
+        private void DownloadGameCompletedCallback(object sender, AsyncCompletedEventArgs e)
+        {
+            try
+            {
+                string onlineVersion = ((Version)e.UserState).ToString();
+                ZipFile.ExtractToDirectory(TermiZip, rootPath, true);
+                File.Delete(TermiZip);
+
+                File.WriteAllText(versionFile, onlineVersion);
+
+                VersionText.Text = onlineVersion;
+                Status = LauncherStatus.ready;
+            }
+            catch (Exception ex)
+            {
+                Status = LauncherStatus.failed;
+                MessageBox.Show($"Error finishing download: {ex}");
+            }
+        }
+
+        private void Window_ContentRendered(object sender, EventArgs e)
+        {
+            CheckForUpdates();
+        }
+
+        private void PlayButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (File.Exists(TermiExe) && Status == LauncherStatus.ready)
+            {
+                ProcessStartInfo startInfo = new ProcessStartInfo(TermiExe);
+                startInfo.WorkingDirectory = Path.Combine(rootPath, "net5.0");
+                Process.Start(startInfo);
+
+                Close();
+            }
+            else if (Status == LauncherStatus.failed)
+            {
+                CheckForUpdates();
+            }
+        }
+    }
+
+    struct Version
+    {
+        internal static Version zero = new Version(0, 0, 0);
+
+        private short major;
+        private short minor;
+        private short subMinor;
+
+        internal Version(short _major, short _minor, short _subMinor)
+        {
+            major = _major;
+            minor = _minor;
+            subMinor = _subMinor;
+        }
+        internal Version(string _version)
+        {
+            string[] versionStrings = _version.Split('.');
+            if (versionStrings.Length != 3)
+            {
+                major = 0;
+                minor = 0;
+                subMinor = 0;
+                return;
+            }
+
+            major = short.Parse(versionStrings[0]);
+            minor = short.Parse(versionStrings[1]);
+            subMinor = short.Parse(versionStrings[2]);
+        }
+
+        internal bool IsDifferentThan(Version _otherVersion)
+        {
+            if (major != _otherVersion.major)
+            {
+                return true;
+            }
+            else
+            {
+                if (minor != _otherVersion.minor)
+                {
+                    return true;
+                }
+                else
+                {
+                    if (subMinor != _otherVersion.subMinor)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public override string ToString()
+        {
+            return $"{major}.{minor}.{subMinor}";
+        }
+    }
+}
