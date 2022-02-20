d3fad354aaaa73ebe5d973cff3d54be914dfb1b2
Andrej Bartulin <andrej.bartulinvr@gmail.com>
2022-02-20 11:08:12 +0100

    Remove non-GUI version of Termi-Windows
    Check branch old for non-GUI version

--- Termi-Windows/Termi/Termi.h
+++
@@ -1,9 +0,0 @@
-
-extern "C" 
-{
-    __declspec(dllexport) void Floppy();
-    __declspec(dllexport) void Calculator(double num1, char operation, double num2);
-    __declspec(dllexport) void GeoCalc();
-    __declspec(dllexport) void Welcome();
-    __declspec(dllexport) void Help();
-}
