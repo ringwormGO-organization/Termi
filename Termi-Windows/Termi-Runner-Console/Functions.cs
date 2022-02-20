93fcda73e3f27a21bf44ac046a6563e9f97defb7
Andrej Bartulin <andrej.bartulinvr@gmail.com>
2022-02-20 12:49:14 +0100

    Put again non-GUI code until GUI version

---
+++ Termi-Windows/Termi-Runner-Console/Functions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Termi_Runner_Console
+{
+    class Functions
+    {
+        public static void Beep()
+        {
+            Console.Beep();
+        }
+
+        public static void Clear()
+        {
+            Console.Clear();
+        }
+
+        public static void Exit()
+        {
+            Environment.Exit(0);
+        }
+
+        public static void Exit(int code)
+        {
+            Environment.Exit(code);
+        }
+    }
+}
---
+++ Termi-Windows/Termi-Runner-Console/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Security.Principal;
+using System.Threading;
+
+namespace Termi_Runner_Console
+{
+    class Program
+    {
+        [DllImport("Termi.dll", EntryPoint = "Welcome", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void Welcome();
+
+        [DllImport("shell32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool IsUserAnAdmin();
+
+        public static string path = Directory.GetCurrentDirectory();
+
+        public static void IsAdministrator()
+        {
+            if (!IsUserAnAdmin())
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("You need to run this application by administrator!");
+                Thread.Sleep(2000);
+                Environment.Exit(0);
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Application have administartor privilges");
+                Thread.Sleep(1000);
+                Console.ResetColor();
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            IsAdministrator();
+
+            Functions.Clear();
+            Console.WriteLine(path + "\n");
+            Console.WriteLine("Starting Termi...\nPlease wait...");
+            Thread.Sleep(1000);
+
+            Functions.Clear();
+
+            Welcome();
+            Input.Input_Start();
+
+            Console.ReadLine();
+        }
+    }
+}
