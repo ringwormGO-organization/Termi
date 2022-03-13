98ac8962d8ef0d2a7b452d053db6272eb47cf540
StjepanBM1 <94230991+StjepanBM1@users.noreply.github.com>
2022-03-13 20:48:14 +0100

    Add files via upload

---
+++ Termi-Linux/Commands/OpenCalc.h
@@ -0,0 +1,42 @@
+#pragma once
+using namespace std;
+#include "Calc.h"
+
+int OpenCalc()
+{
+    char op;
+    float num1, num2;
+
+    cout << "Enter operator: +, -, *, /: ";
+    cin >> op;
+
+    cout << "Enter the first number: ";
+    cin >> num1;
+    cout << "Enter the second number: ";
+    cin >> num2;
+
+    switch(op) 
+    {
+        case '+':
+            cout << num1 << " + " << num2 << " = " << num1 + num2 << endl;
+            break;
+
+        case '-':
+            cout << num1 << " - " << num2 << " = " << num1 - num2 << endl;
+            break;
+
+        case '*':
+            cout << num1 << " * " << num2 << " = " << num1 * num2 << endl;
+            break;
+
+        case '/':
+            cout << num1 << " / " << num2 << " = " << num1 / num2 << endl;
+            break;
+
+        default:
+            // If the operator is other than +, -, * or /, error message is shown
+            cout << "Error! operator is not correct";
+            break;
+    }
+    return 0;
+}
---
+++ Termi-Linux/main.cpp
@@ -0,0 +1,161 @@
+/**
+ * @author Stjepan Bilić Matišić
+ * PROJECT: Termi-Linux version
+ * LICENSE: BSD-3-Clause-License
+ * DESCRIPTION: Main file for Linux Version
+*/
+
+// IMPORTANT STUFF
+////////////////////////////////////////////////////////////////////////////////////////
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <string.h>
+#include <stdlib.h>
+#include <ctime>
+#include <fstream>
+#include <exception>
+#include <memory>
+#include <filesystem>
+#include <sys/stat.h>
+#include <signal.h>
+#include <stdio.h>
+#include <dirent.h>
+#include <sys/types.h>
+
+//#include "Commands/Calc.h"
+#include "Commands/Filesys.h"
+#include "Commands/GeoCalc.h"
+#include "Commands/OpenCalc.h"
+
+#define MAX 1000
+#define MAX_FILE 25
+
+#define VERSION_MAJOR "0"
+#define VERSION_MINOR "1"
+#define VERSION_PATCH "4"
+#define VERSION "v" VERSION_MAJOR "." VERSION_MINOR "." VERSION_PATCH
+
+using namespace std;
+
+////////////////////////////////////////////////////////////////////////////////////////
+// Andrej's update for Termi-Linux was a mess.
+// As a result some functions have been removed until further notice
+// Thank you for understanding
+// -StjepanBM1
+
+/* Functions */
+
+
+/* Structs */
+struct sigaction sigIntHandler;
+
+/* variable for string input */
+std::string input;
+
+// Fucntion which control keyboards events (Ctrl-C for example)
+void end(int sig)
+{
+    std::cout << "\nPress any key to continue...\n";
+    auto key = std::cin.get();
+    if (key != 10)
+    {
+        /* we need to do something here; input is broken */
+        exit(0);
+    }
+    else
+    {
+        exit(sig);
+    }
+}
+
+int main(void)
+{
+
+    cout << "ooooooooooo                              " << endl;
+    cout << "    888      ooooooooooo                          o88   " << endl;
+    cout << "    888      888    88  oo oooooo  oo ooo oooo   oooo  " << endl;
+    cout << "    888      888ooo8     888    888 888 888 888   888 " << endl;
+    cout << "    888      888    oo   888        888 888 888   888  " << endl;
+    cout << "   o888o    o888ooo8888 o888o      o888o888o888o o888o " << endl;
+    cout << "------------------------------------------------------- " << endl;
+    cout << "           TYPE help TO SEE LIST OF ALL COMMANDS" << endl;
+    cout << "          (C)2022 ringwormGO All rights reserved" << endl;
+    cout << "-------------------------------------------------------  " << endl;
+
+    // command loop
+    while (1)
+    {
+        cout << "Termi> ";
+        getline(cin, input);
+
+        // Strings / Commands
+        string help = "help";
+        string exit = "exit";
+        string clear = "clear";
+        string filesys = "filesys";
+        string list = "list";
+        string opencalc = "opencalc";
+        string geocalc = "geocalc";
+
+        // Catch CTRL-C
+        sigIntHandler.sa_handler = end;
+        sigemptyset(&sigIntHandler.sa_mask);
+        sigIntHandler.sa_flags = 0;
+        sigaction(SIGINT, &sigIntHandler, NULL);
+
+
+        if (input == help)
+        {
+            cout << "help - shows list of commands" << endl;
+            cout << "opencalc - opens a calculator" << endl;
+            cout << "geocalc - opens a geometric calculator" << endl;
+            cout << "clear or cls - clear console" << endl;
+            cout << "exit - closes Termi" << endl;
+            cout << "----------FILESYS COMMANDS" << endl;
+            cout << "filesys - opens file creation/editing" << endl;
+            cout << "filesys / openfile - opens a file" << endl;
+            cout << "filesys / writefile - create and edit a file" << endl;
+            cout << "filesys / mkdir - make a directory" << endl;
+            cout << "filesys / cd - cd into a directory" << endl;
+            cout << "filesys / rm - removes a file" << endl;
+        }
+
+        else if (input == filesys)
+        {
+            Filesys();
+        }
+
+        else if (input == opencalc)
+        {
+            OpenCalc();                
+        }
+
+
+        else if (input == geocalc)
+        {
+            GeoCalc();
+        }
+
+        else if (input == clear || input == "cls")
+        {
+            cout << "\033c";
+        }
+
+        else if (input == exit)
+        {
+            return(0);
+        }
+
+        else if (input.length() == 0) /* enter */
+        {
+
+        }
+
+        else
+        {
+            std::cout << "'" << input << "'" << "is invalid command!\n";
+        }
+    }
+}
