3b2d038b2cda7aec03003a0e9ae4f968cd7e21ac
Andrej <andrej.bartulinvr@gmail.com>
2022-03-17 16:26:38 +0100

    Linux debugging

--- Termi-Linux/compile.sh
+++ Termi-Linux/compile.sh
@@ -5,13 +5,13 @@ rm -rf ./bin/filesys
 rm -rf ./bin/GeoCalc
 rm -rf ./bin/neofetch
 
-g++ -o bin/OpenCalc Commands/OpenCalc.cpp
-g++ -o bin/filesys Commands/Filesys.cpp
-g++ -o bin/GeoCalc Commands/GeoCalc.cpp
-g++ -o bin/neofetch Commands/Neofetch.cpp
+g++ -g -o bin/OpenCalc Commands/OpenCalc.cpp
+g++ -g -o bin/filesys Commands/Filesys.cpp
+g++ -g -o bin/GeoCalc Commands/GeoCalc.cpp
+g++ -g -o bin/neofetch Commands/Neofetch.cpp
 
 clear
 
 rm -rf ./Termi
-g++ -o Termi main.cpp
+g++ -g -o Termi main.cpp
 ./Termi
