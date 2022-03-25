afdab495a08fd7355a9ae76837560640679488c1
StjepanBM1 <stjepan.bilicmatsic546@gmail.com>
2022-03-25 20:20:21 +0100

    tlib, Co-authored-by: Andrej Bartulin <andrej.bartulinvr@gmail.com>

---
+++ tlib/test.cpp
@@ -0,0 +1,17 @@
+/**
+ * @author Andrej Bartulin
+ * @author Stjepan Bilić Matišić
+ * PROJECT: tlib test
+ * LICENSE: BSD-3-Clause-License
+ * DESCRIPTION: Main file for tlib test
+*/
+
+#include "tlib.h"
+
+//using namespace std;
+
+int main()
+{
+    //cout << "Compilation uspjesna" << endl;
+    return 0;
+}
