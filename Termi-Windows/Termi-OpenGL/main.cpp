f766d7f9a950f32490f22bd9f6d62b101ac24294
Andrej Bartulin <andrej.bartulinvr@gmail.com>
2022-08-19 19:00:03 +0200

    Windows version sync

--- Termi-Windows/Termi-OpenGL/main.cpp
+++ Termi-Windows/Termi-OpenGL/main.cpp
@@ -15,7 +15,7 @@
 #include <iostream>
 #include <string>
 #include <fstream>
-#include <Windows.h>
+#include <signal.h>
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
@@ -30,154 +30,186 @@ int height;
 
 static BOOL WINAPI end(DWORD dwCtrlType)
 {
-	int key;
+    int key;
 
-	switch (dwCtrlType)
-	{
-		case CTRL_C_EVENT: // Ctrl+C
-			std::cout << "\nPress any key to continue...\n";
-			key = std::cin.get();
-			if (key != 10)
-			{
-				/* we need to do something here; input is broken */
-				exit(0);
-			}
-			else
-			{
-				exit(0);
-			}
-			break;
-		case CTRL_BREAK_EVENT: // Ctrl+Break
-			break;
-		case CTRL_CLOSE_EVENT: // Closing the console window
-			break;
-		case CTRL_LOGOFF_EVENT: // User logs off. Passed only to services!
-			break;
-		case CTRL_SHUTDOWN_EVENT: // System is shutting down. Passed only to services!
-			break;
-	}
+    switch (dwCtrlType)
+    {
+        case CTRL_C_EVENT: // Ctrl+C
+            std::cout << "\nPress any key to continue...\n";
+            key = std::cin.get();
+            if (key != 10)
+            {
+                /* we need to do something here; input is broken */
+                exit(0);
+            }
+            else
+            {
+                exit(0);
+            }
+            break;
+        case CTRL_BREAK_EVENT: // Ctrl+Break
+            break;
+        case CTRL_CLOSE_EVENT: // Closing the console window
+            break;
+        case CTRL_LOGOFF_EVENT: // User logs off. Passed only to services!
+            break;
+        case CTRL_SHUTDOWN_EVENT: // System is shutting down. Passed only to services!
+            break;
+    }
 
-	// Return TRUE if handled this message, further handler functions won't be called.
-	// Return FALSE to pass this message to further handlers until default handler calls ExitProcess().
-	return FALSE;
+    // Return TRUE if handled this message, further handler functions won't be called.
+    // Return FALSE to pass this message to further handlers until default handler calls ExitProcess().
+    return FALSE;
 }
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
-	std::cout << "\n\n";
+    std::cout << "\n\n";
 
-	bool arg = false;
-	bool alreadyarg = false;
+    Renderer* render = new Renderer();
 
-	if (argc > 1)
-	{
-		render.startup_command = argv[1];
-		arg = true;
-	}
+    bool arg = false;
+    bool alreadyarg = false;
+    bool iconReady = false;
 
-	/* Catch CTRL-C */
-	SetConsoleCtrlHandler(end, TRUE);
+    if (argc > 1)
+    {
+        render->startup_command = argv[1];
+        arg = true;
+    }
 
-	std::cout << "ooooooooooo                              " << std::endl;
-	std::cout << "    888      ooooooooooo                          o88   " << std::endl;
-	std::cout << "    888      888    88  oo oooooo  oo ooo oooo   oooo  " << std::endl;
-	std::cout << "    888      888ooo8     888    888 888 888 888   888 " << std::endl;
-	std::cout << "    888      888    oo   888        888 888 888   888  " << std::endl;
-	std::cout << "   o888o    o888ooo8888 o888o      o888o888o888o o888o " << std::endl;
-	std::cout << "------------------------------------------------------- " << std::endl;
+    /* Catch CTRL-C */
+    SetConsoleCtrlHandler(end, TRUE);
 
-	glfwInit();
-	GLFWwindow* window = glfwCreateWindow(render.Settings(1, 0), render.Settings(2, 0), "Termi (OpenGL)", NULL, NULL);
-	glfwMakeContextCurrent(window);
-	gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
-	if (window == NULL)
-	{
-		std::cout << "Unable to create OpenGL window!\nExiting...\n";
-		return 1;
-	}
+    std::cout << "ooooooooooo                              " << std::endl;
+    std::cout << "    888      ooooooooooo                          o88   " << std::endl;
+    std::cout << "    888      888    88  oo oooooo  oo ooo oooo   oooo  " << std::endl;
+    std::cout << "    888      888ooo8     888    888 888 888 888   888 " << std::endl;
+    std::cout << "    888      888    oo   888        888 888 888   888  " << std::endl;
+    std::cout << "   o888o    o888ooo8888 o888o      o888o888o888o o888o " << std::endl;
+    std::cout << "------------------------------------------------------- " << std::endl;
 
-	else
-	{
-		#ifdef PRINT_WHEN_WINDOW_IS_CREATED
-			std::cout << "OpenGL window is created.\n";
-		#endif
-	}
+    if (render->CheckFile("termi.png") == false)
+    {
+        std::cout << "Icon wasn't found!\n";
+        std::cout << "Do you want download it [y (requires curl) /n]? : " << std::endl;
 
-	GLFWimage images[1];
-	images[0].pixels = stbi_load("termi.png", &images[0].width, &images[0].height, 0, 4); //rgba channels 
-	glfwSetWindowIcon(window, 1, images);
-	stbi_image_free(images[0].pixels);
+        std::string yn;
+        std::cin >> yn;
 
-	/* Initialize Dear ImGui */
-	IMGUI_CHECKVERSION();
-	ImGui::CreateContext();
-	ImGuiIO& io = ImGui::GetIO(); (void)io;
-	ImGui::StyleColorsDark();
-	ImGui_ImplGlfw_InitForOpenGL(window, true);
-	ImGui_ImplOpenGL3_Init("#version 330");
+        if (yn == "y")
+        {
+            system("wget https://raw.githubusercontent.com/ringwormGO-organization/Termi/main/Termi-Linux-OpenGL/termi.png");
 
-	render.Settings(3, 0);
+            if (render->CheckFile("termi.png"))
+            {
+                iconReady = true;
+            }
+        }
+    }
 
-	if (render.font_name != "default") /* idk why != now works */
-	{
-		try
-		{
-			if (render.CheckFile(render.font_name.c_str()) == 0)
-			{
-				io.Fonts->AddFontFromFileTTF(render.font_name.c_str(), render.Settings(4, 0));
-			}
-		}
-		catch (const std::exception& e)
-		{
-			std::cerr << "Exception catched! Result: " << e.what() << '\n';
-		}
-	}
+    else
+    {
+        iconReady = true;
+    }
 
-	render.Settings(0, 0);
+    glfwInit();
+    GLFWwindow* window = glfwCreateWindow(render->Settings(1, 0), render->Settings(2, 0), "Termi (OpenGL)", NULL, NULL);
+    glfwMakeContextCurrent(window);
+    gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+    if (window == NULL)
+    {
+        std::cout << "Unable to create OpenGL window!\nExiting...\n";
+        return 1;
+    }
 
-	if (render.startup_command != "none")
-	{
-		arg = true;
-	}
+    else
+    {
+        #ifdef PRINT_WHEN_WINDOW_IS_CREATED
+                std::cout << "OpenGL window is created.\n";
+        #endif
+    }
 
-	while(!glfwWindowShouldClose(window))
-	{
-		glClear(GL_COLOR_BUFFER_BIT);
+    if (iconReady)
+    {
+        GLFWimage images[1];
+        images[0].pixels = stbi_load("termi.png", &images[0].width, &images[0].height, 0, 4); /* rgba channels */
+        glfwSetWindowIcon(window, 1, images);
+        stbi_image_free(images[0].pixels);
+    }
 
-		/* Tell OpenGL a new frame is about to begin */
-		ImGui_ImplOpenGL3_NewFrame();
-		ImGui_ImplGlfw_NewFrame();
-		ImGui::NewFrame();
+    else
+    {
+        std::cout << "Continuing without an icon!\n";
+    }
 
-		glfwGetWindowSize(window, &width, &height);
-		window_width = static_cast<float>(width);
-		window_height = static_cast<float>(height);
+    /* Initialize Dear ImGui */
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    ImGui::StyleColorsDark();
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 330");
 
-		ImGui::SetNextWindowPos(ImVec2(pos_x, pos_y));
-    	ImGui::SetNextWindowSize(ImVec2(window_width, window_height));
+    render->Settings(3, 0);
 
-		/* main Dear ImGui code */
-		main_code();
+    if (render->font_name != "default")
+    {
+        if (render->CheckFile(render->font_name.c_str()) == 1)
+        {
+            io.Fonts->AddFontFromFileTTF(render->font_name.c_str(), render->Settings(4, 0));
+        }
+    }
 
-		if (arg && !alreadyarg)
-		{
-			console.ExecCommand(render.startup_command, argv[1]);
-			alreadyarg = true;
-		}
+    render->Settings(0, 0);
 
-		#ifdef PRINT_FPS
-			printf("Application average %.3f ms/frame (%.1f FPS)\r", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-		#endif
+    if (render->startup_command != "none")
+    {
+        arg = true;
+    }
 
-		/* Renders the Dear ImGui elements */
-		ImGui::Render();
-		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+    while (!glfwWindowShouldClose(window))
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
 
-		glfwSwapBuffers(window);
-		glfwPollEvents();
-	}
+        /* Tell OpenGL a new frame is about to begin */
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
 
-	glfwTerminate();
+        glfwGetWindowSize(window, &width, &height);
+        window_width = static_cast<float>(width);
+        window_height = static_cast<float>(height);
 
-	return 0;
+        ImGui::SetNextWindowPos(ImVec2(pos_x, pos_y));
+        ImGui::SetNextWindowSize(ImVec2(window_width, window_height));
+
+        render = new Renderer();
+
+        /* main Dear ImGui code */
+        main_code(render);
+
+        if (arg && !alreadyarg)
+        {
+            console.ExecCommand(render->startup_command, argv[2]);
+            alreadyarg = true;
+        }
+
+        #ifdef PRINT_FPS
+                printf("Application average %.3f ms/frame (%.1f FPS)\r", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+        #endif
+
+        /* Renders the Dear ImGui elements */
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+
+        delete render;
+    }
+
+    glfwTerminate();
+
+    return 0;
 }
