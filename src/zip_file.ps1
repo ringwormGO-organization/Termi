1b451ed22ce2f0607fa425aaf0bf183f66586495
Andrej Bartulin <andrej.bartulinvr@gmail.com>
2022-12-20 20:26:15 +0100

    Use release mode

--- src/zip_file.ps1
+++ src/zip_file.ps1
@@ -1,20 +1,20 @@
 clear
 cd ..\
 
-$Folder = 'windows-release\Debug'
-$File = 'windows-release\Debug\Debug.zip'
+$Folder = 'windows-release\Release'
+$File = 'windows-release\Release\Release.zip'
 
 if (Test-Path -Path $Folder) {
-    Remove-Item 'windows-release\Debug\' -Recurse
+    Remove-Item 'windows-release\Release\' -Recurse
 }
 
 if (Test-Path -Path $File) {
-    Remove-Item 'windows-release\Debug.zip' -Recurse
+    Remove-Item 'windows-release\Release.zip' -Recurse
 }
 
-mkdir windows-release\Debug\
+mkdir windows-release\Release\
 
-Copy-Item 'src\Termi-Main\build\Debug\*' -Destination 'windows-release\Debug\'
-Compress-Archive -Path windows-release\Debug -DestinationPath windows-release\Debug.zip
+Copy-Item 'src\Termi-Main\build\Release\*' -Destination 'windows-release\Release\'
+Compress-Archive -Path windows-release\Release -DestinationPath windows-release\Release.zip
 
 cd src
