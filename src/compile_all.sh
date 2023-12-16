d3855de2ede4be5f7a291feb4ba6f7d0f2c4a816
Someone <hahahahacker2009@airmail.cc>
2023-12-16 17:55:24 +0700

    support multithread make!

--- src/compile_all.sh
+++ src/compile_all.sh
@@ -1,20 +1,25 @@
 clear
-
+if [ -z $1 ]; then
+    export threads=1
+else
+    export threads=$1
+fi
+echo "Number of threads: ${threads}"
 cd Termi-Commands
 cmake --build . --target clean
-cmake . && make
+cmake . && make -j ${threads}
 cp libTermi-Commands.so ../Termi-Main
 echo " "
 
 cd ../Termi-GUI
 cmake --build . --target clean
-cmake . && make
+cmake . && make -j ${threads}
 cp libTermi-GUI.so ../Termi-Main
 echo " "
 
 cd ../Termi-Main
 cmake --build . --target clean
-cmake . && make
+cmake . && make -j ${threads}
 
 echo " "
 LD_LIBRARY_PATH=. ./Termi-Main
