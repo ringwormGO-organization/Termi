f9a8ee563c9336f080e214dbcc2be1b322438acb
Someone <hahahahacker2009@airmail.cc>
2023-12-16 13:31:36 +0700

    clean first then compile, no cache

--- src/compile_all.sh
+++ src/compile_all.sh
@@ -1,16 +1,19 @@
 clear
 
 cd Termi-Commands
+cmake --build . --target clean
 cmake . && make
 cp libTermi-Commands.so ../Termi-Main
 echo " "
 
 cd ../Termi-GUI
+cmake --build . --target clean
 cmake . && make
 cp libTermi-GUI.so ../Termi-Main
 echo " "
 
 cd ../Termi-Main
+cmake --build . --target clean
 cmake . && make
 
 echo " "
