36b1ebe92ac0cd823ec01aee6feb43ec180446db
Someone <hahahahacker2009@airmail.cc>
2023-12-16 18:55:27 +0700

    update default number of threads to 2 to match most modern hardware; don't use export

--- src/compile_all.sh
+++ src/compile_all.sh
@@ -1,25 +1,26 @@
+#!/bin/sh
 clear
 if [ -z $1 ]; then
-    export threads=1
+    threads=2
 else
-    export threads=$1
+    threads=$1
 fi
-echo "Number of threads: ${threads}"
+echo "Number of threads: $threads"
 cd Termi-Commands
 cmake --build . --target clean
-cmake . && make -j ${threads}
+cmake . && make -j $threads
 cp libTermi-Commands.so ../Termi-Main
 echo " "
 
 cd ../Termi-GUI
 cmake --build . --target clean
-cmake . && make -j ${threads}
+cmake . && make -j $threads
 cp libTermi-GUI.so ../Termi-Main
 echo " "
 
 cd ../Termi-Main
 cmake --build . --target clean
-cmake . && make -j ${threads}
+cmake . && make -j $threads
 
 echo " "
 LD_LIBRARY_PATH=. ./Termi-Main
