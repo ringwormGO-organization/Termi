e07ce39f83902d4fe8449946d6aa4e027a1628c8
Bùi Nguyễn Tấn Sang <endermatday@gmail.com>
2023-12-17 20:52:11 +0700

    Update translation.hpp
    Add more language.

--- src/Termi-GUI/translation.hpp
+++ src/Termi-GUI/translation.hpp
@@ -15,7 +15,9 @@ namespace Translation
     static std::vector<std::string> English = {"Input", "Terminal", "Edit", "About", "New Tab", "New profile", "Exit", "Change theme (light/dark)", "Change language", "About Termi", "About Dear ImGui", "Remote connection", "Server", "Client", "Change theme"};
     static std::vector<std::string> Croatian = {"Unos", "Terminal", "Uredi", "O Termiju", "Nova kartica", "Novi profil", "Izađi", "Promijeni temu (svijetla/tamna)", "Promijeni jezik", "O Termiju", "O Dear ImGui-u", "Veza na daljinu", "Server", "Klijent", "Promijeni temu"};
     static std::vector<std::string> Vietnamese = {"Nhập vào", "Dòng lệnh", "Chỉnh sửa", "Giới thiệu", "Thẻ mới", "Hồ sơ mới", "Thoát", "Đổi chủ đề nền (sáng/tối)", "Đổi ngôn ngữ", "Về Termi", "Về Dear ImGui", "Kết nối", "Máy chủ", "Máy khách", "Đổi chủ đề nền"};
-    // static std::vector<std::string> Vietnamese = {"Nhap vao", "Dong lenh", "Chinh sua", "Gioi thieu" "The moi", "Ho so moi", "Thoat", "Doi chu de nen (sang/toi)", "Doi ngon ngu", "Ve Termi", "Ve Dear ImGui", "Ket noi", "May chu", "May khach", "Doi chu de nen"};
+    static std::vector<std::string> German = {"Eingabe", "Terminal", "Bearbeiten", "Über", "Neuer Tab", "Neues Profil", "Beenden", "Thema wechseln (hell/dunkel)", "Sprache ändern", "Über Termi", "Über Dear ImGui", "Fernverbindung", "Server", "Client", "Thema wechseln"};
+    static std::vector<std::string> Spanish = {"Entrada", "Terminal", "Editar", "Acerca de", "Nueva pestaña", "Nuevo perfil", "Salir", "Cambiar tema (claro/oscuro)", "Cambiar idioma", "Acerca de Termi", "Acerca de Dear ImGui", "Conexión remota", "Servidor", "Cliente", "Cambiar tema"};
+    static std::vector<std::string> Esperanto = {"Enigo", "Terminalo", "Redakti", "Pri", "Nova langeto", "Nova profilo", "Eliri", "Ŝanĝi temon (luma/malhelma)", "Ŝanĝi lingvon", "Pri Termi", "Pri Dear ImGui", "Fora konekto", "Servilo", "Kliento", "Ŝanĝi temon"};
     // Without native Vietnamese character
     /**
      * Naming convention for IDs fot strings
